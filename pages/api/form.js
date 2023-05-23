ba632a5503aa6a0d4c28d26852d2abf9e978fc0e
Aidan <datbluemushroom@gmail.com>
2023-05-23 14:02:17 -0400

    🧪

--- pages/api/form.js
+++ pages/api/form.js
@@ -30,7 +30,7 @@ const handler = (req, res) => {
         At the end of the modified resume, include a section named "Most Relavent Skills for ${job} Position" that includes the most important skills for ${job} from the original resume along with the titles of the experiences and description of how those skills are used in the experiences provided in the original resume. Everything must be in Markdown format. Thank you for your hard work!`,
 
         model: "text-davinci-003",
-        max_tokens: 200,
+        max_tokens: 400,
         temperature: 0,
     };
 
