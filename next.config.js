bb6ebc8695e343aaaa4334f80e6a303bbd9ddd05
Aidan <datbluemushroom@gmail.com>
2023-05-28 19:11:12 -0400

    🔥 🔥

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-var ProtobufPlugin = require('protobufjs-webpack-plugin');
-
-console.log(ProtobufPlugin);
-
 const nextConfig = {
-  webpack: (config, options) => {
-    config.plugins.push(
-      new ProtobufPlugin({})
-    )
-    return config;
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
