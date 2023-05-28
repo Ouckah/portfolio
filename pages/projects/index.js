125eea28053456c97230673225693a7b2aec8319
Aidan <datbluemushroom@gmail.com>
2023-05-28 16:12:49 -0400

    💄 Project Info Updates

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -20,6 +20,13 @@ export default function Projects()
                 <div className="flex justify-center w-full h-full py-5">
                     <div className='grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 gap-24 justify-center md:px-20'>
                         <ProjectCard 
+                            title="Gridder"
+                            description="A fun grid puzzle game I made in highschool!"
+                            image="https://img.itch.zone/aW1nLzgxNjAwOTcucG5n/315x250%23c/2oiljW.png"
+                            date="March 19th, 2022"
+                            link='gridder'
+                        />
+                        <ProjectCard 
                             title="Tic Tac Toe"
                             description="A fully working tic tac toe board built with React!"
                             image="https://web.stevens.edu/news/newspoints/zoom-bg/Stevens-zoom-background-garden.jpg"
@@ -27,16 +34,9 @@ export default function Projects()
                             link='tictactoe'
                         />
                         <ProjectCard 
-                            title="Gridder"
-                            description="A fun grid puzzle game I made in highschool!"
-                            image="https://img.itch.zone/aW1nLzgxNjAwOTcucG5n/315x250%23c/2oiljW.png"
-                            date="May 5th, 2023"
-                            link='gridder'
-                        />
-                        <ProjectCard 
                             title="Resume Re-Writer"
                             description="AI that rewrites your resume based on your requested job niche."
-                            image="https://img.itch.zone/aW1nLzgxNjAwOTcucG5n/315x250%23c/2oiljW.png"
+                            image="https://media.istockphoto.com/id/638584374/vector/writer-writing-on-paper-sheet-workplace-author-desktop-write-letter.jpg?s=612x612&w=0&k=20&c=_OM7rmAP12TuzdY4WXekh_rVJMNzpcCNUDqVqmU52TI="
                             date="May 17th, 2023"
                             link='resume'
                         />
